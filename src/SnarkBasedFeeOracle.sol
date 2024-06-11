0f1e81ce1386452e334fa8302c4f5b7dfca0086f
pedro bufulin <pedro@semiotic.ai>
2024-06-10 21:09:36 -0300

    snark based fee oracle

---
+++ src/SnarkBasedFeeOracle.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {RvVerifier} from "./RvVerifier.sol";
+
+contract SnarkBasedFeeOracle is RvVerifier {
+    bytes public n1_inv;
+    uint256 public s2;
+    uint256 rv;
+
+    constructor(bytes32 _programKey) RvVerifier(_programKey) {}
+
+    /// @notice The entrypoint for verifying the proof and updating state variables.
+    /// @param proof The encoded proof.
+    /// @param publicValues The encoded public values.
+    function verifyAndUpdate(
+        bytes memory proof,
+        bytes memory publicValues
+    ) public {
+        // Verify the proof using the base contract's function
+        (, , bytes memory new_n1_inv, bytes memory new_s2, ) = this.verifyRvProof(proof, publicValues);
+
+        // Update the state variables
+
+        s2 = uint256(bytes32(new_s2));
+
+        // TODO: used fixed point floats for rv and substitue 10 by ln(1.0001)
+        // TODO: Also, there are two values that are input to the proof, n_inv_sqrt and n_inv 
+        // and one additional value output from the proof n_bytes. 
+        // We need to decode n_bytes to get n and then check that n_inv * n == 1 and n_inv_sqrt * n.sqrt() == 1.
+
+        rv = s2 * 10;
+
+    }
+
+    function getVolatility() public view returns (uint256) {
+        return rv;
+    }
+
+
+
+
+}
