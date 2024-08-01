0f31e01326a8b4f3e3d7ea1659e19c281b8d7429
pedro bufulin <pedro@semiotic.ai>
2024-08-01 17:19:30 -0300

    feat(scripts/deployCalcLib): adds script to deploy calc lib and replace the previous calclib from hook

---
+++ script/DeployCalcLib.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "forge-std/console.sol";
+
+import {CalcFeeLib} from "src/Calc/CalcFeeLib.sol";
+import {Script} from "forge-std/Script.sol";
+import {OracleBasedFeeHook} from "src/OracleBasedFeeHook.sol";
+
+// Deploys and sets the new calclib contract
+// and sets the CalcLib into the OracleBasedHook (assuming the current deployer is the owner of the hook)
+contract DeployCalcFeeLib is Script {
+    function run() external {
+        address RvOracleAddress = 0xa120424BdC490002F0B949e2BB461302547e6769;
+        OracleBasedFeeHook hook = OracleBasedFeeHook(0x01acEbd7a1117b3D52C6089D9df296F323D12080);
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address deployer = vm.rememberKey(deployerPrivateKey);
+        address deployerAddress = vm.addr(deployerPrivateKey);
+
+        console.log("Deploying contract with the deployer address:", deployerAddress);
+
+        vm.startBroadcast(deployer);
+        CalcFeeLib calcFeeLib = new CalcFeeLib(RvOracleAddress);
+
+        hook.setCalcLib(address(calcFeeLib));
+
+        vm.stopBroadcast();
+
+        console.log("CalcFeeLib deployed to:", address(calcFeeLib));
+
+        // sets the new calcFeeLib address int the OracleBasedHook
+
+
+    }
+}
